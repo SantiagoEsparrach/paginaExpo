0ee91c8dc8a9eb3c779712a1fb8c6c33276403ee
SantiagoEsparrach <santiesparrachperlo@gmail.com>
2024-06-24 07:56:46 -0300

    bb

--- back/express-gen-ts/src/models/User.ts
+++ back/express-gen-ts/src/models/User.ts
@@ -5,8 +5,7 @@ import moment from 'moment';
 
 const INVALID_CONSTRUCTOR_PARAM = 'nameOrObj arg must a string or an object ' + 
   'with the appropriate user keys.';
-
-
+  
 // **** Types **** //
 
 export interface IUser {
